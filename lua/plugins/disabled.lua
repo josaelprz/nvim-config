bc399b5f50ae56acd577b747cddc2dbb65271ae3
JosaelPrz <josael.prz@gmail.com>
2024-04-27 17:37:14 -0700

    Initial commit

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,70 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    "which-key.nvim",
+    enabled = false,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
+    "echasnovski/mini.bufremove",
+    enabled = false,
+  },
+  {
+    "folke/persistence.nvim",
+    enabled = false,
+  },
+  {
+    "gbprod/yanky.nvim",
+    enabled = false,
+  },
+  {
+    "stevearc/aerial.nvim",
+    enabled = false,
+  },
+  {
+    "monaqa/dial.nvim",
+    enabled = false,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    enabled = false,
+  },
+  {
+    "ggandor/flit.nvim",
+    enabled = false,
+  },
+  {
+    "ggandor/leap.nvim",
+    enabled = false,
+  },
+  {
+    "echasnovski/mini.diff",
+    enabled = false,
+  },
+  {
+    "echasnovski/mini.files",
+    enabled = false,
+  },
+  {
+    "hedyhli/outline.nvim",
+    enabled = false,
+  },
+  {
+    "ahmedkhalf/project.nvim",
+    enabled = false,
+  },
+  {
+    "folke/edgy.nvim",
+    enabled = false,
+  },
+}
