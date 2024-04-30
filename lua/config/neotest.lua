dbb9f69137899dd9dba75bb93c5063deaba15b10
JosaelPrz <josael.prz@gmail.com>
2024-04-29 22:44:12 -0700

    Add neotest with jest for testing

---
+++ lua/config/neotest.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-neotest/neotest-jest",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-jest")({
+            jestCommand = "npm test --",
+            jestConfigFile = "jest.config.ts",
+            env = { CI = true },
+            cwd = function()
+              return vim.fn.getcwd()
+            end,
+          }),
+        },
+      })
+    end,
+  },
+}
