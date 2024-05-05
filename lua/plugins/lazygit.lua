c5aeb35c1e7e1edb0d7b131ec252ca48f2b19859
JosaelPrz <josael.prz@gmail.com>
2024-05-05 11:51:12 -0700

    Set lazygit panel transparency

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -16,4 +16,8 @@ return {
   keys = {
     { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
   },
+  config = function()
+    vim.g.lazygit_floating_window_winblend = 20 -- transparency of floating window
+    vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
+  end,
 }
