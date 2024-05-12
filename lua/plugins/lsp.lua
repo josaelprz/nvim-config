bac118f2619944a9711b072d3c808a34b432a727
JosaelPrz <josael.prz@gmail.com>
2024-05-12 15:11:15 -0700

    Add tailwindcss language server support inside class variance authorization (cva)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,4 +5,18 @@ return {
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "<S-k>", mode = { "n" }, false }
   end,
+  config = function()
+    require("lspconfig").tailwindcss.setup({
+      settings = {
+        tailwindCSS = {
+          experimental = {
+            classRegex = {
+              { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+              { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+            },
+          },
+        },
+      },
+    })
+  end,
 }
