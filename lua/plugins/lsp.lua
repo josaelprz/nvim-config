453763ed07552fc21e3304e5f7e58cbd0b62719c
JosaelPrz <josael.prz@gmail.com>
2024-05-12 15:41:58 -0700

    Fix lsp server not working due to tailwind-cva config overriding lsp default config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,8 +4,7 @@ return {
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "<S-k>", mode = { "n" }, false }
-  end,
-  config = function()
+
     require("lspconfig").tailwindcss.setup({
       settings = {
         tailwindCSS = {
