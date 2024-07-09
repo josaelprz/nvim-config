8c40df9eab95c995ff02fc9dc7afeac7926430d5
JosaelPrz <josael.prz@gmail.com>
2024-07-09 04:10:44 -0700

    Configure SQL dadbod autocompletion

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local cmp = require("cmp")
+
+-- autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sql", "mysql", "plsql" },
+  callback = function()
+    cmp.setup.buffer({
+      sources = {
+        { name = "vim-dadbod-completion" },
+      },
+    })
+  end,
+})
