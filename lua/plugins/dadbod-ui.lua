37ff43329421357eeac02d2347d25df24fd774fa
JosaelPrz <josael.prz@gmail.com>
2024-07-10 04:10:51 -0700

    Setup dadbod to use neovim notifications

--- lua/plugins/dadbod-ui.lua
+++ lua/plugins/dadbod-ui.lua
@@ -4,4 +4,7 @@ return {
     { "tpope/vim-dadbod", lazy = true },
     { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
   },
+  init = function()
+    vim.g.db_ui_use_nvim_notify = true
+  end,
 }
