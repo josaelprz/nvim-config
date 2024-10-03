e0005d6d3413defc3924a0691d84132d4c95a2cd
JosaelPrz <josael.prz@gmail.com>
2024-10-03 15:33:10 -0700

    Update plugins to be managed by LazyVim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,4 +18,39 @@ return {
       },
     })
   end,
+  opts = {
+    servers = {
+      tsserver = {
+        enabled = false,
+      },
+      vtsls = {
+        settings = {
+          complete_function_calls = true,
+          vtsls = {
+            enableMoveToFileCodeAction = true,
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = false },
+              parameterNames = { enabled = false },
+              parameterTypes = { enabled = false },
+              propertyDeclarationTypes = { enabled = false },
+              variableTypes = { enabled = false },
+            },
+          },
+        },
+      },
+    },
+  },
 }
