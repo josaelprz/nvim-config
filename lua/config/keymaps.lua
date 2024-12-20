0eb276f2e74cfe0570fe9887b68ce5ec985c59dd
josaelprz <josael.prz@gmail.com>
2024-12-19 19:19:30 -0800

    Update fuzzy finding keymaps to use fzf instead of telescope (deprecated)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -123,101 +123,36 @@ vim.opt.smartcase = true
 vim.opt.hlsearch = true
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-local builtin = require("telescope.builtin")
-
--- Search by word
-map("n", "<leader>sw", builtin.grep_string)
+local fzf = require("fzf-lua")
 
 -- Search in multiple files with grep
-map("n", "<Leader>sg", builtin.live_grep)
+map("n", "<Leader>sg", fzf.live_grep)
 
 -- Search files
-map("n", "<Leader>sf", builtin.find_files)
-
--- Search open files
-map("n", "<Leader>so", builtin.buffers)
-
--- Search diagnostics
-map("n", "<leader>sd", builtin.diagnostics)
-
--- Search keymaps
-map("n", "<leader>sk", builtin.keymaps)
+map("n", "<Leader>sf", fzf.files)
 
 -- Search help
-map("n", "<leader>sh", builtin.help_tags)
+map("n", "<leader>sh", fzf.help_tags)
 
 -- Find & Replace
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-  callback = function(event)
-    local builtin = require("telescope.builtin")
-    local opts = { buffer = event.buf }
+-- Show options about the word under your cursor
+map("n", "<Leader>o", vim.lsp.buf.hover)
 
-    -- Show options about the word under your cursor
-    map("n", "<Leader>o", vim.lsp.buf.hover, opts)
+-- Code action
+map("n", "<Leader>.", vim.lsp.buf.code_action)
 
-    -- Code action
-    map("n", "<Leader>.", vim.lsp.buf.code_action, opts)
+-- Rename symbol under your cursor
+map("n", "<Leader>rs", vim.lsp.buf.rename)
 
-    -- Rename symbol under your cursor
-    map("n", "<Leader>rs", vim.lsp.buf.rename, opts)
+-- Go to definition of the word under your cursor
+map("n", "<Leader>gd", vim.lsp.buf.definition)
 
-    -- Go to definition of the word under your cursor
-    -- map("n", "<Leader>gd", vim.lsp.buf.definition, opts)
-    map("n", "<Leader>gd", builtin.lsp_definitions, opts)
+-- Go to type definition of the word under your cursor.
+map("n", "<Leader>gt", vim.lsp.buf.type_definition)
 
-    -- Go to type definition of the word under your cursor.
-    -- map('n', '<Leader>gt', vim.lsp.buf.type_definition, opts)
-    map("n", "<Leader>gt", builtin.lsp_type_definitions, opts)
-
-    -- Go to declaration of the word under your cursor
-    -- Useful when language allow forward declarations
-    map("n", "<Leader>gD", vim.lsp.buf.declaration, opts)
-
-    -- Go to implementation of the word under your cursor
-    -- map("n", "<Leader>gi", vim.lsp.buf.implementation, opts)
-    map("n", "<Leader>gi", builtin.lsp_implementations, opts)
-
-    -- Find references for the word under your cursor.
-    map("n", "<Leader>gr", builtin.lsp_references, opts)
-
-    -- Fuzzy find all the symbols in your current document.
-    map("n", "<Leader>ss", builtin.lsp_document_symbols, opts)
-
-    -- Fuzzy find all the symbols in your current workspace.
-    map("n", "<Leader>sS", builtin.lsp_dynamic_workspace_symbols, opts)
-
-    -- The following two autocommands are used to highlight references of the word under your cursor
-    -- when your cursor rests there for a little while. When you move your cursor, the highlights will be cleared
-    -- See :help CursorHold for information about when this is executed
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-    if client and client.server_capabilities.documentHighlightProvider then
-      local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        buffer = event.buf,
-        group = highlight_augroup,
-        callback = vim.lsp.buf.document_highlight,
-      })
-
-      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-        buffer = event.buf,
-        group = highlight_augroup,
-        callback = vim.lsp.buf.clear_references,
-      })
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd("LspDetach", {
-  group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-  callback = function(event)
-    vim.lsp.buf.clear_references()
-    vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event.buf })
-  end,
-})
+-- Go to implementation of the word under your cursor
+map("n", "<Leader>gi", vim.lsp.buf.implementation)
 
 -- NOTE: EXPLORER
 local explorer = require("oil")
@@ -242,14 +177,3 @@ map("n", "<leader>ts", test.run.stop)
 map("n", "<leader>to", test.summary.toggle)
 map("n", "<leader>tm", test.output_panel.toggle)
 map("n", "<leader>tc", test.output_panel.clear)
-
--- NOTE: LSP COMPLETIONS
-local cmp = require("cmp")
-map({ "n", "i" }, "<A-j>", cmp.select_next_item)
-map({ "n", "i" }, "<A-k>", cmp.select_prev_item)
-map({ "n", "i" }, "<C-Space>", cmp.complete)
-
--- NOTE: SQL UI (DADBOD & DADBOD UI)
-map("n", "<leader>db", "<cmd>DBUIToggle<CR>")
-map("n", "<leader>W", "<Plug>(DBUI_SaveQuery)")
-map({ "n", "v" }, "<leader>r", "<Plug>(DBUI_ExecuteQuery)")
